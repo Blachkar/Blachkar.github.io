9bfa758a207927888419620198d5a22089b32add
Blachkar <131146989+Blachkar@users.noreply.github.com>
2023-04-19 13:35:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,20 @@ window.addEventListener('load', () => {
     loader.classList.add('fondu-out');
     
 })
+
+const pagecontent = document.querySelector('h1')
+
+new Typewriter(pagecontent, {
+    //loop: true,
+})
+.changeDelay(50)
+.typeString('Hey. <stron>Je suis</strong>')
+.pauseFor(450)
+.typeString('<strong> <br>Lachakr Bilal</strong>.')
+.pauseFor(450)
+.deleteChars(14)
+.typeString('<span style="color: #ad8ded">Lachakr</span> <strong>Bilal</strong>')
+.pauseFor(450)
+.deleteChars(5)
+.typeString('<span style="color: #393939">Bilal.</span>')
+.start()
